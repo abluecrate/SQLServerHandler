8205d3a1eee546cf296b64a84f83aa5c1449fe4f
Ethan Walkowiak <ethanwalkowiak@gmail.com>
2019-03-20 09:39:42 -0400

    added print statements

--- sqlsrvrHandler.py
+++ sqlsrvrHandler.py
@@ -10,12 +10,17 @@ class SQLServer():
         self.connection = self.connect()
 
     def connect(self):
+        print('[LOG] Connecting to SQL Server')
+        print('      Server:   {}'.format(self.SERVER))
+        print('      Database: {}'.format(self.DATABASE))
+
         return pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                                 "Server=" + self.SERVER + ";"
                                 "Database=" + self.DATABASE + ";"
                                 "Trusted_Connection=yes;")
 
     def queryToDF(self, query, chunksize = None):
+        print('[LOG] Reading SQL Query')
         return pd.read_sql_query(query, self.connection, chunksize = chunksize)
 
 #-------------------------------------------------------------------
